77312bbcf1128829a12dc071392946bb695101c0
LuisLuisLuisLuisLu <luis9_22@alu.uabcs.mx>
2025-02-13 15:15:14 -0700

    Add files via upload

---
+++ ventana/main.java
@@ -0,0 +1,16 @@
+package ventana;
+
+import javax.swing.JPanel;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		ventana v1 = new ventana("Tarea");
+		v1.setVisible(true);
+		
+		
+		
+	}
+
+}
